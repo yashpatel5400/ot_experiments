7735284c198e888426ef99a38f001f8290ea9eec
yashpatel5400 <ypatel5400@gmail.com>
2022-01-28 10:15:51 -0500

    I'm less confused but still confused

--- basic.py
+++ basic.py
@@ -11,18 +11,26 @@ n = 100  # nb bins
 x = np.arange(n, dtype=np.float64)
 
 # Gaussian distributions
-a = np.random.normal(20, 5, 1000)
-a_hist, bins_edges = np.histogram(a, bins=30)
-a_prob = a_hist / np.linalg.norm(a_hist)
-bins = [(bins_edges[i] + bins_edges[i+1]) / 2 for i in range(len(bins_edges) - 1)]
+# a = np.random.normal(20, 5, 1000)
+# a_hist, bins_edges = np.histogram(a, bins=30)
+# a_prob = a_hist / np.linalg.norm(a_hist)
+# bins = [(bins_edges[i] + bins_edges[i+1]) / 2 for i in range(len(bins_edges) - 1)]
 
-b = gauss(n, m=20, s=5)  # m= mean, s= std
-# b = gauss(n, m=60, s=10)
+a = gauss(n, m=20, s=5)  # m= mean, s= std
+b = gauss(n, m=60, s=10)
+
+print(x)
 
 # loss matrix
-# M = ot.dist(x.reshape((n, 1)), x.reshape((n, 1)))
-# M /= M.max()
+M = ot.dist(x.reshape((n, 1)), x.reshape((n, 1)))
+M /= M.max()
 
-plt.plot(bins, a_prob) # true notion of empirical PDF
-plt.plot(x, b)
+# plt.plot(bins, a_prob) # true notion of empirical PDF
+# plt.plot(x, b)
+# plt.show()
+
+G0 = ot.emd(a, b, M)
+
+pl.figure(3, figsize=(5, 5))
+ot.plot.plot1D_mat(a, b, G0, 'OT matrix G0')
 plt.show()
