fcbab8d9c88241475cef979ebcfd3e8345fb283c
yashpatel5400 <ypatel5400@gmail.com>
2022-05-11 16:04:13 -0400

    Mountain car seems to work

--- mountain_car.py
+++ mountain_car.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
 env = gym.make('MountainCar-v0')
 env.reset()
@@ -8,10 +9,10 @@ max_state = env.observation_space.high
 min_state = env.observation_space.low
 
 state_space_size = 2
-state_buckets = [10] * state_space_size
+state_buckets = [20] * state_space_size
 state_deltas = [(max_state[i] - min_state[i]) / state_buckets[i] for i in range(state_space_size)]
 
-episodes = 1000
+episodes = 2000
 alpha = 0.1
 epsilon = 0.9
 gamma = 0.9
@@ -27,18 +28,21 @@ def eps_greedy_action(Q, state, epsilon):
     s1, s2 = discretize_state(state)
     return np.argmax(Q[s1, s2, :])
 
+rewards = []
+
 for episode in range(episodes):
     if episode > 0 and episode % 100 == 0:
+        print(episode)
         epsilon /= 2
 
     state = env.reset()
     action = eps_greedy_action(Q, state, epsilon)
     done = False
 
+    episode_reward = 0
     while not done:
-        if episode % 50 == 0:
-            print(episode)
-            env.render()
+        # if episode % 50 == 0:
+        #     env.render()
 
         new_state, reward, done, _ = env.step(action)
         new_action = eps_greedy_action(Q, new_state, epsilon)
@@ -51,3 +55,17 @@ for episode in range(episodes):
 
         state = new_state
         action = new_action
+
+        episode_reward += reward
+    rewards.append(episode_reward)
+
+# final "evaluation" run
+done = False
+state = env.reset()
+while not done:
+    env.render()
+    action = eps_greedy_action(Q, state, 0)
+    state, _, done, _ = env.step(action)
+
+plt.plot(range(len(rewards)), rewards)
+plt.show()
