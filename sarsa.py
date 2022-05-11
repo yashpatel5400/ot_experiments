0a24482fa0f4346a7d767f364140ad9ffb5dbd6d
yashpatel5400 <ypatel5400@gmail.com>
2022-05-11 15:34:40 -0400

    Still not working

--- sarsa.py
+++ sarsa.py
@@ -9,51 +9,27 @@ env = gym.make('CartPole-v0')
 env.reset()
 
 episodes = 1000
-gamma = 0.1
-eps = 0.1
-
-# construct feature extractor
-observation_examples = []
-for _ in range(2500):
-    state, _, done, _ = env.step(env.action_space.sample())
-    print(state)
-    if done:
-    	env.reset()
-    observation_examples.append(state)
-
-observation_examples = np.array([observation_examples]).squeeze()
-scaler = sklearn.preprocessing.StandardScaler()
-scaler.fit(observation_examples)
-
-# Used to convert a state to a featurized representation.
-# We use RBF kernels with different variances to cover different parts of the space
-featurizer = sklearn.pipeline.FeatureUnion([
-        ("rbf1", RBFSampler(gamma=5.0, n_components=100)),
-        ("rbf2", RBFSampler(gamma=2.0, n_components=100)),
-        ("rbf3", RBFSampler(gamma=1.0, n_components=100)),
-        ("rbf4", RBFSampler(gamma=0.5, n_components=100))
-        ])
-featurizer.fit(scaler.transform(observation_examples))
-
-def featurize_state(state):
-    scaled = scaler.transform([state])
-    featurized = featurizer.transform(scaled)
-    return featurized[0]
+gamma = 1.0
+eps = 1.0
 
 
 def eps_greedy(epsilon, state, Qs):
 	if np.random.random() < epsilon:
 		return env.action_space.sample()
-	pred_Qs = [Q.predict([featurize_state(state)]) for Q in Qs]
+	pred_Qs = [Q.predict([state]) for Q in Qs]
 	return np.argmax(pred_Qs)
 
 # sorta hacky, but having one Q per A allows us to very directly find the best action
 # by just iterating over the A and comparing Q(s). in cont action spaces, we would optimize over A
 state = env.reset()
-Qs = [SGDRegressor() for _ in range(env.action_space.n)]
-[Q.partial_fit([featurize_state(state)], [0]) for Q in Qs]
+Qs = [SGDRegressor(learning_rate="constant") for _ in range(env.action_space.n)]
+[Q.partial_fit([state], [0]) for Q in Qs]
 
-for _ in range(episodes):
+for episode in range(episodes):
+	if episode > 0 and episode % 100 == 0:
+		eps /= 2
+		print(f"epsilon={eps}")
+
 	env.render() # no render on train
 	done = False
 
@@ -64,9 +40,9 @@ for _ in range(episodes):
 		new_state, reward, done, _ = env.step(action)
 		new_action = eps_greedy(eps, new_state, Qs)
 		
-		td_target = gamma * Qs[new_action].predict([featurize_state(new_state)]) + reward \
-			- Qs[action].predict([featurize_state(state)])
-		Qs[action].partial_fit([featurize_state(state)], [td_target])
+		td_target = gamma * Qs[new_action].predict([new_state]) + reward \
+			- Qs[action].predict([state])
+		Qs[action] = Qs[action].partial_fit([state], np.array([td_target]).ravel())
 		
 		state = new_state
 		action = new_action
